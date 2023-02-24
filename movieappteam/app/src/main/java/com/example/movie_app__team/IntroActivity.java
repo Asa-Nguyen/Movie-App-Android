d806a16a1d8f6baf0c4d37f400ae9ffc0a07090f
HydraCoder <ngodat.it213@gmail.com>
2023-02-24 23:20:52 +0700

    update btn click

--- movieappteam/app/src/main/java/com/example/movie_app__team/IntroActivity.java
+++ movieappteam/app/src/main/java/com/example/movie_app__team/IntroActivity.java
@@ -1,6 +1,9 @@
 package com.example.movie_app__team;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,15 @@ public class IntroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        Button click=(Button)findViewById(R.id.get_started);
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(IntroActivity.this, SignInActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
--- movieappteam/app/src/main/java/com/example/movie_app__team/SignInActivity.java
+++ movieappteam/app/src/main/java/com/example/movie_app__team/SignInActivity.java
@@ -1,6 +1,9 @@
 package com.example.movie_app__team;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,13 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        Button click=(Button)findViewById(R.id.loginBtn);
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignInActivity.this, HomefullActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
