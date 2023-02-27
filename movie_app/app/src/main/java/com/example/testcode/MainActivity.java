e1380516449666b340f90a12c367e718f886ef56
HydraCoder <ngodat.it213@gmail.com>
2023-02-27 23:25:06 +0700

    update getstarted screen
    xong

--- movie_app/app/src/main/java/com/example/testcode/MainActivity.java
+++ movie_app/app/src/main/java/com/example/testcode/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.testcode;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
