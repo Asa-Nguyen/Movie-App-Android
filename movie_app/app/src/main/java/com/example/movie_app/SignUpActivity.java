c64f0f0765b25e855c3db4a87fbe48a8b26b12c0
HydraCoder <ngodat.it213@gmail.com>
2023-03-07 11:54:43 +0700

    update logo image

--- movie_app/app/src/main/java/com/example/movie_app/SignUpActivity.java
+++ movie_app/app/src/main/java/com/example/movie_app/SignUpActivity.java
@@ -34,7 +34,7 @@ public class SignUpActivity extends AppCompatActivity {
         click.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(SignUpActivity.this, HomeFullActivity.class);
+                Intent intent = new Intent(SignUpActivity.this, SignInActivity.class);
                 startActivity(intent);
 
                 //        Test fire base
